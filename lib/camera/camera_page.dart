41dc0f91a894409809dee0af7f675c5097f5a498
nauvalachid <nauvalachid@gmail.com>
2025-05-22 20:20:11 +0700

    menambahkan halaman CameraPage dengan implementasi dasar

---
+++ lib/camera/camera_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget{
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
