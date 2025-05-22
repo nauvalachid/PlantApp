cb0de0098cf0e16b023a71bcd2ca16fd4eb93e83
nauvalachid <nauvalachid@gmail.com>
2025-05-22 16:42:46 +0700

    Menambahkan build method TitleWithMoreBtn dengan judul dan tombol ElevatedButton

--- lib/screens/home/components/title_with_more_bbtn.dart
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
 
 class TitleWithMoreBtn extends StatelessWidget {
   const TitleWithMoreBtn({
@@ -9,4 +10,25 @@ class TitleWithMoreBtn extends StatelessWidget {
 
   final String title;
   final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          TitleWithCustomUnderline(text: title),
+          const Spacer(),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
