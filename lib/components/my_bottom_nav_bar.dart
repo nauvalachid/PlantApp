6aa465b5ae2217468b45857d0f09e83442d055f8
nauvalachid <nauvalachid@gmail.com>
2025-05-22 16:58:42 +0700

    Menambahkan build method MyBottomNavBar dengan styling container dan shadow

--- lib/components/my_bottom_nav_bar.dart
+++ lib/components/my_bottom_nav_bar.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
 
 class MyBottomNavBar extends StatelessWidget {
   const MyBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: kDefaultPadding * 2,
+        right: kDefaultPadding * 2,
+        bottom: kDefaultPadding,
+      ),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, -10),
+            blurRadius: 35,
+            color: kPrimaryColor.withOpacity(0.38),
+          ),
+        ],
+      ),
+    );
+  }
 }
