9cb6a1f2754b9571653812c85a66993e180464c0
nauvalachid <nauvalachid@gmail.com>
2025-05-22 17:46:21 +0700

    Menambahkan implementasi build method pada kelas TitleAndPrice untuk menampilkan judul dengan padding dan styling

--- lib/screens/details/components/title_and_price.dart
+++ lib/screens/details/components/title_and_price.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
 
 class TitleAndPrice extends StatelessWidget {
   const TitleAndPrice({
@@ -11,4 +12,28 @@ class TitleAndPrice extends StatelessWidget {
   final String title;
   final String country;
   final int price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "$title\n",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                        color: kTextColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
