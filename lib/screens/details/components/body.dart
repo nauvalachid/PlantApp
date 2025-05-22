2291c30480d0cbe67ea73e53a3be61109ad20e56
nauvalachid <nauvalachid@gmail.com>
2025-05-22 17:49:54 +0700

    Menambahkan implementasi kelas Body untuk menampilkan konten utama pada layar detail

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/details/components/image_and_icons.dart';
+import 'package:plantapp/screens/details/components/title_and_price.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          const SizedBox(height: kDefaultPadding),
+          const TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          const SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
 }
