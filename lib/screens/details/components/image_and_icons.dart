b426164106a83a0dcaa156d1e0b6a3e07f4ef5ce
nauvalachid <nauvalachid@gmail.com>
2025-05-22 17:37:31 +0700

    Menambahkan kelas ImageAndIcons dengan properti size

---
+++ lib/screens/details/components/image_and_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImageAndIcons extends StatelessWidget {
+  const ImageAndIcons({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+}
