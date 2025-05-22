82fccc4fbfdfd064b6aaaebff14dbb72f00b1cec
nauvalachid <nauvalachid@gmail.com>
2025-05-22 17:45:10 +0700

    Menambahkan kelas TitleAndPrice untuk menampilkan judul dan harga

---
+++ lib/screens/details/components/title_and_price.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TitleAndPrice extends StatelessWidget {
+  const TitleAndPrice({
+    super.key,
+    required this.title,
+    required this.country,
+    required this.price,
+  });
+
+  final String title;
+  final String country;
+  final int price;
+}
