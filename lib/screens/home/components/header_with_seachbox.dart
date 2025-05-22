d40ac05b39eaa8a85eb28231da3ca1d673f57d49
nauvalachid <nauvalachid@gmail.com>
2025-05-22 15:54:53 +0700

    Menambahkan kelas HeaderWithSearchBox dengan properti size

---
+++ lib/screens/home/components/header_with_seachbox.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HeaderWithSearchBox extends StatelessWidget {
+  const HeaderWithSearchBox({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+}
