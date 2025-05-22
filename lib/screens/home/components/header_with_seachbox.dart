eb55f6e11320e41805184677683e771fe93ad94e
nauvalachid <nauvalachid@gmail.com>
2025-05-22 15:58:31 +0700

    Menambahkan build method pada HeaderWithSearchBox dengan styling dan layout dasar

--- lib/screens/home/components/header_with_seachbox.dart
+++ lib/screens/home/components/header_with_seachbox.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
 
 class HeaderWithSearchBox extends StatelessWidget {
   const HeaderWithSearchBox({
@@ -7,4 +8,31 @@ class HeaderWithSearchBox extends StatelessWidget {
   }) : super(key: key);
 
   final Size size;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      // It will cover 20% of our total height
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.2 - 27,
+            decoration: const BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
