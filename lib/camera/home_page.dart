33342c8ba8b10dceb449888097fe3f8da1465505
nauvalachid <nauvalachid@gmail.com>
2025-05-23 00:31:49 +0700

    refactor: simplify image handling and enhance UI in FullPage

--- lib/camera/home_page.dart
+++ lib/camera/home_page.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
-import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:plantapp/camera/camera_page.dart';
 import 'package:plantapp/camera/storage_helper.dart';
+import 'package:plantapp/constants.dart';
 
 class FullPage extends StatefulWidget {
   const FullPage({super.key});
@@ -32,44 +32,18 @@ class _FullPageState extends State<FullPage> {
       setState(() => _imageFile = saved);
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text('Disimpan: ${saved.path}'))); 
-      }
-    }
-
-    Future<void> _pickFromGallery() async {
-      final picker = ImagePicker();
-      final picked = await picker.pickImage(source: ImageSource.gallery);
-      if (picked != null) {
-        final saved = await StorageHelper.saveImage(File(picked.path), 'gallery');
-        setState(() => _imageFile = saved);
-        ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text('Disalin: ${saved.path}')));
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Beranda')),
+      appBar: AppBar(title: const Text('Camera', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+      centerTitle: true,),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton.icon(
-                    icon: const Icon(Icons.camera),
-                    label: const Text('Ambil Foto'),
-                    onPressed: _takePicture,
-                  ),
-                ),
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.folder),
-                  label: const Text('Pilih dari Galeri'),
-                  onPressed: _pickFromGallery,
-                ),
-              ],
-            ),
             const SizedBox(height: 20),
             if (_imageFile != null)
               Column(
@@ -81,13 +55,17 @@ class _FullPageState extends State<FullPage> {
                       width: double.infinity, 
                     ),
                   ),
-                  Text(
-                    'Gambar disimpan di: ${_imageFile?.path ?? 'Tidak ada'}',
-                    style: const TextStyle(fontSize: 12),
-                  ),
+                  const SizedBox(height: 8),
                   ElevatedButton.icon(
-                    icon: const Icon(Icons.delete),
-                    label: const Text('Hapus Gambar'),
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    label: const Text(
+                      'Hapus Gambar',
+                      style: TextStyle(color: Colors.white, fontSize: 14 ,fontWeight: FontWeight.bold),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      
+                      backgroundColor: kPrimaryColor,
+                    ),
                     onPressed: () async {
                       await _imageFile?.delete();
                       setState(() => _imageFile = null);
@@ -95,11 +73,44 @@ class _FullPageState extends State<FullPage> {
                         const SnackBar(content: Text('Gambar dihapus')));
                     },
                   ),
+                  const SizedBox(height: 16),
                 ],
               )
             else
-              const Text('Belum ada gambar diambil/dipilih.'),
-            const SizedBox(height: 20),
+               const Expanded(
+                  child: Center(
+                    child: Text(
+                      'Belum ada gambar diambil/dipilih.',
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+            const Spacer(),
+            Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                icon: const Icon(Icons.camera , color: Colors.white),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kPrimaryColor,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                label: const Text('Ambil Foto',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: _takePicture,
+              ),
+            ),
+            ),
           ],
         ),
       ),
