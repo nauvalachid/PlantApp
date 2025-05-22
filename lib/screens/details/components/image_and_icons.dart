5efc93c7faf31800ef98b13fd694564ccfe8e4b9
nauvalachid <nauvalachid@gmail.com>
2025-05-22 17:39:40 +0700

    Menambahkan layout dasar ImageAndIcons dengan tombol kembali dan padding

--- lib/screens/details/components/image_and_icons.dart
+++ lib/screens/details/components/image_and_icons.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plantapp/constants.dart';
 
 class ImageAndIcons extends StatelessWidget {
   const ImageAndIcons({
@@ -7,4 +9,39 @@ class ImageAndIcons extends StatelessWidget {
   });
 
   final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+      child: SizedBox(
+        height: size.height * 0.8,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 3),
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: kDefaultPadding),
+                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
