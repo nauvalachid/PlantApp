4e58b9e4deb101c0e1ae0e1e568c8d7c2ec7fd2a
nauvalachid <nauvalachid@gmail.com>
2025-05-22 15:45:47 +0700

    tampilkan daftar horizontal tanaman unggulan

--- lib/screens/home/components/featurred_plants.dart
+++ lib/screens/home/components/featurred_plants.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
 
 class FeaturedPlants extends StatelessWidget {
   const FeaturedPlants({
     Key? key,
   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: _onCardPressed,
+          ),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: _onCardPressed,
+          ),
+        ],
+      ),
+    );
+  }
+  static void _onCardPressed() {
+    // TODO: Ganti dengan navigasi atau aksi sesuai kebutuhan
+    debugPrint("Feature plant card tapped");
+  }
 }
