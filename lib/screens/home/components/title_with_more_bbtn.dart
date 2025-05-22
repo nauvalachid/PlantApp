49af89fcf0c8922415748149a09825c77e3fc89e
nauvalachid <nauvalachid@gmail.com>
2025-05-22 16:41:31 +0700

    Menambahkan kelas TitleWithMoreBtn dengan properti title dan aksi press

---
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final VoidCallback press;
+}
