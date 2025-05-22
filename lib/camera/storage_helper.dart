4bf84dac6afc11b5cf40c1a6ad90daf7377bb38c
nauvalachid <nauvalachid@gmail.com>
2025-05-22 20:32:38 +0700

    mengganti versi ndk & pembenaran typo

--- lib/camera/storage_helper.dart
+++ lib/camera/storage_helper.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart' as path;
 class StorageHelper {
   static Future<String> _getFolderPath() async {
     final Directory dir =
-        Directory('/strorage/emulated/0/DCIM/FlutterNativeCam');
+        Directory('/storage/emulated/0/DCIM/FlutterNativeCam');
     if (!await dir.exists()) await dir.create(recursive: true);
     return dir.path;
   }
