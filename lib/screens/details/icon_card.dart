382ac9e3580295435b79f957b88435db7eed25ef
nauvalachid <nauvalachid@gmail.com>
2025-05-22 17:33:29 +0700

    Menambahkan kelas IconCard dengan properti icon

---
+++ lib/screens/details/icon_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IconCard extends StatelessWidget {
+  const IconCard({
+    Key? key,
+    required this.icon,
+  }) : super(key: key);
+
+  final String icon;
+}
