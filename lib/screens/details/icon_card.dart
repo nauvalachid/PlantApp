23621b56ac0712317ba9fa214594de7b415e2b3f
nauvalachid <nauvalachid@gmail.com>
2025-05-22 17:34:39 +0700

    Menambahkan build method IconCard dengan styling container dan efek bayangan

--- lib/screens/details/icon_card.dart
+++ lib/screens/details/icon_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
 
 class IconCard extends StatelessWidget {
   const IconCard({
@@ -7,4 +8,32 @@ class IconCard extends StatelessWidget {
   }) : super(key: key);
 
   final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
+      padding: const EdgeInsets.all(kDefaultPadding / 2),
+      height: 62,
+      width: 62,
+      decoration: BoxDecoration(
+        color: kBackgroundColor,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 15),
+            blurRadius: 22,
+            color: kPrimaryColor.withOpacity(0.22),
+          ),
+          const BoxShadow(
+            offset: Offset(-15, -15),
+            blurRadius: 20,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
 }
