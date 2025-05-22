a2910ac734bd8a0577d5edf275f51cb480038860
nauvalachid <nauvalachid@gmail.com>
2025-05-22 16:16:15 +0700

    Menambahkan build method pada RecomendsPlants dengan scroll horizontal dan satu RecomendPlantCard

--- lib/screens/home/components/recomend_plants.dart
+++ lib/screens/home/components/recomend_plants.dart
@@ -4,4 +4,29 @@ class RecomendsPlants extends StatelessWidget {
   const RecomendsPlants({
     Key? key,
   }) : super(key: key);
+
+ @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          RecomendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) => const DetailsScreen(),
+              //   ),
+              // );
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
